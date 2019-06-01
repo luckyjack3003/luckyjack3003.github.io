4f8b62d5054181d3fad6b7a6dd3349e5d353a53a
Jack Gu <jack@mansol.net.au>
2019-06-01 17:32:56 +1000

    ck push

---
+++ ck/precache-manifest.6b1c17281a88d36cf7f9d7d920b5cc13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc2453b06c20a49105c558e93a581f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d314c0e4ae2770690f51",
+    "url": "/static/css/2.611aae94.chunk.css"
+  },
+  {
+    "revision": "fe4ac38a917a1218fd90",
+    "url": "/static/css/main.58465852.chunk.css"
+  },
+  {
+    "revision": "d314c0e4ae2770690f51",
+    "url": "/static/js/2.be69aaa7.chunk.js"
+  },
+  {
+    "revision": "fe4ac38a917a1218fd90",
+    "url": "/static/js/main.37d46dcd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
